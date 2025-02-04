61d2d103b8a542e2490dc8a7c97f8cad935b0ecd
Stefan Bilharz <stefan@sbsoftware.de>
2025-02-04 21:40:34 +0100

    HIT-12 Add box shadow to home page grid

--- src/views/home_view.cr
+++ src/views/home_view.cr
@@ -8,6 +8,7 @@ class HomeView < Crumble::ContextView
 
     rule HomeViewGrid > GameGridView::Grid do
       prop("transform", "perspective(500px) rotateX(45deg) translate(0px, -100px)")
+      prop("box-shadow", "0px 50px 30px 20px rgba(30, 30, 30, 0.5)")
     end
 
     rule HomeViewGrid > GameGridView::Cell do
