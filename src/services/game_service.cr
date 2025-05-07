9986056cb73fa06c5885569e4be97aa0a388e79f
Stefan Bilharz <stefan@sbsoftware.de>
2025-05-07 17:40:54 +0200

    HIT-32 Ensure no two consecutive HitTargets have the same position

--- src/services/game_service.cr
+++ src/services/game_service.cr
@@ -15,11 +15,26 @@ class GameService
       GamePlayer.create(game_id: game.id, session_id: waiting_player.session_id, player_name: waiting_player.player_name)
     end
 
-    Game::GAME_DURATION.total_seconds.to_i.times do |i|
-      HitTarget.create(game_id: game.id, pos_x: rand(1..game.size_x.value), pos_y: rand(1..game.size_y.value), delay_ms: 1000 * i)
+    generate_target_positions(Game::GAME_DURATION.total_seconds.to_u32, game.size_x.value.to_u32, game.size_y.value.to_u32) do |(pos_x, pos_y), i|
+      HitTarget.create(game_id: game.id, pos_x: pos_x.to_i, pos_y: pos_y.to_i, delay_ms: 1000 * i.to_i)
     end
     # TODO: Transaction end
 
     game
+  end
+
+  def self.generate_target_positions(count : UInt32, size_x : UInt32, size_y : UInt32, &block : (Tuple(UInt32, UInt32), UInt32) -> Nil) : Nil
+    i = 0_u32
+    last = nil
+
+    while (i < count)
+      positions = {rand(1_u32..size_x), rand(1_u32..size_y)}
+
+      next if last && last == positions
+
+      yield positions, i
+      last = positions
+      i += 1
+    end
   end
 end
