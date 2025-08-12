01e31ba969f2df4b6b3d00b77859747490634941
Stefan Bilharz <stefan@sbsoftware.de>
2025-08-12 21:10:17 +0200

    HIT-40 Use existing feature

--- src/crumble_server.cr
+++ src/crumble_server.cr
@@ -74,12 +74,12 @@ spawn do
       unless (last_game_at = room.last_game_started_at) && last_game_at > 3.minutes.ago
         room.room_players.each do |room_player|
           if (last_check = room_player.last_connection_check_at) && last_check < 60.seconds.ago
-            room_player.db.exec("DELETE FROM #{room_player.table_name} WHERE id=#{room_player.id}")
+            room_player.destroy
           end
         end
 
         if room.room_players.empty?
-          room.db.exec("DELETE FROM #{room.table_name} WHERE id=#{room.id}")
+          room.destroy
         end
       end
 
