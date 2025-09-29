3c6cd8f75f6a693b8052ba94bf2d9b0641b69b06
Stefan Bilharz <stefan@sbsoftware.de>
2025-09-29 20:12:38 +0200

    HIT-45 Remove duplicate macros

--- src/environment.cr
+++ src/environment.cr
@@ -1,17 +1,5 @@
-macro css_class(name)
-  class {{name}} < CSS::CSSClass; end
-end
-
 macro element_id(name)
   class {{name}} < CSS::ElementId; end
-end
-
-macro style(&blk)
-  class Style < CSS::Stylesheet
-    rules do
-      {{blk.body}}
-    end
-  end
 end
 
 require "sqlite3"
